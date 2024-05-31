e2c6ddad894d246abdee78db729372edc33ca04c
abiassantana <abias.santana1@gmail.com>
2024-05-30 23:38:24 -0300

    Commit inicial do systema

---
+++ src/main/java/com/desafio/pitang/desafio_pitang/DesafioPitangApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.pitang.desafio_pitang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPitangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPitangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/pitang/desafio_pitang/DesafioPitangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.pitang.desafio_pitang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPitangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
